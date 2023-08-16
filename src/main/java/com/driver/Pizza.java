cfa67727eb3fd4423a62bde7d41d13e93103f4fd
Anshul <anshulgayakwad12345@gmail.com>
2023-08-16 16:52:45 +0530

    1st Attemp

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,12 +3,23 @@ package com.driver;
 public class Pizza {
 
     private int price;
-    private Boolean isVeg;
-    private String bill;
+    private final Boolean isVeg;
+    private String bill = "";
+    private boolean isExtraCheeseAdded = false;
+    private boolean isExtraToppingAdded = false;
+    private boolean isTakeAwayAdded = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            price = 300;
+            bill += "Base Price Of The Pizza: 300\n";
+        }
+        else{
+            price = 400;
+            bill += "Base Price Of The Pizza: 400\n";
+        }
     }
 
     public int getPrice(){
@@ -17,18 +28,49 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!isExtraCheeseAdded) {
+            price += 80;
+            isExtraCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!isExtraToppingAdded) {
+            if (isVeg) {
+                price += 70;
+            } else {
+                price += 120;
+            }
+            isExtraToppingAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!isTakeAwayAdded) {
+            price += 20;
+            isTakeAwayAdded = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        if(isExtraCheeseAdded){
+            bill += "Extra Cheese Added: 80\n";
+        }
+        if(isExtraToppingAdded){
+            if(isVeg){
+                bill += "Extra Toppings Added: 70\n";
+            }
+            else{
+                bill += "Extra Toppings Added: 120\n";
+            }
+        }
+        if(isTakeAwayAdded){
+            bill += "Paper bag Price = 20\n";
+        }
+        bill += "Total Price: "+price;
         return this.bill;
     }
 }
